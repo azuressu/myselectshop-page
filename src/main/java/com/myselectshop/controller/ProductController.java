3a007fe299ab21493209a10bf59e708e94d93d9f
azuressu <martina0130@naver.com>
2025-02-03 20:05:41 +0900

    :sparkles: 상품 페이징 및 정렬

--- src/main/java/com/myselectshop/controller/ProductController.java
+++ src/main/java/com/myselectshop/controller/ProductController.java
@@ -30,12 +30,13 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return productService.getProducts(userDetails.getUser());
-    }
-
-    @GetMapping("/admin/products")
-    public List<ProductResponseDto> getAllProducts() {
-        return productService.getAllProducts();
+    public List<ProductResponseDto> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.getProducts(userDetails.getUser(),
+                page-1, size, sortBy, isAsc);
     }
 }
--- src/main/java/com/myselectshop/service/ProductService.java
+++ src/main/java/com/myselectshop/service/ProductService.java
@@ -5,9 +5,14 @@ import com.myselectshop.dto.ProductRequestDto;
 import com.myselectshop.dto.ProductResponseDto;
 import com.myselectshop.entity.Product;
 import com.myselectshop.entity.User;
+import com.myselectshop.entity.UserRoleEnum;
 import com.myselectshop.naver.dto.ItemDto;
 import com.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,18 +49,23 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
-    public List<ProductResponseDto> getProducts(User user) {
-        return productRepository.findAllByUser(user).stream().map(ProductResponseDto::new).toList();
-        /*
-         List<Product> productList = productRepository.findAll();
-         List<ProductResponseDto> productDtoList = new ArrayList<>();
+    public List<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
 
-         for (Product product: productList) {
-            productDtoList.add(new ProductResposneDto(product));
-         }
+        Pageable pageable = PageRequest.of(page, size, sort);
 
-         return productDtoList;
-         */
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        Page<Product> productList;
+
+        if (userRoleEnum == UserRoleEnum.USER) {
+            productList = productRepository.findAllByUser(user, pageable);
+        } else {
+            productList = productRepository.findAll(pageable);
+        }
+
+        return productList.map(ProductResponseDto::new).toList();
     }
 
     @Transactional
@@ -63,9 +73,5 @@ public class ProductService {
         Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("해당 상품은 존재하지 않습니다."));
 
         product.updateByItemDto(itemDto);
-    }
-
-    public List<ProductResponseDto> getAllProducts() {
-        return productRepository.findAll().stream().map(ProductResponseDto::new).toList();
     }
 }
